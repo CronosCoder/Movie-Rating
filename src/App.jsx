3763ffc51c5710ff0bf5ee15655bc8cab78151da
Foysal Ahmed <mfoysal314@gmail.com>
2024-04-02 02:28:48 +0600

    project initialized with necessary packages

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <Button>Hello this is Movie Rating app</Button>
+    </>
+  )
+}
+
+export default App
