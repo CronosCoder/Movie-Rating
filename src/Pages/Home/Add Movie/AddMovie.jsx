a31ce01afcc087828c0ae2fda67453ce9fde2bc0
Foysal Ahmed <mfoysal314@gmail.com>
2024-04-04 01:57:53 +0600

    add movie section added

--- src/Pages/Home/Add Movie/AddMovie.jsx
+++ src/Pages/Home/Add Movie/AddMovie.jsx
@@ -1,16 +1,39 @@
+import { allMovieUrl } from "@/Utilities/Url";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Swal from 'sweetalert2'
 
 const AddMovie = () => {
     const [name,setName] = useState("");
     const [genre, setGenre] = useState("");
     const [rating, setRating] = useState("");
     const [release, setRelease] = useState("");
+    const accessToken = localStorage.getItem("accessToken");
+    const navigate = useNavigate();
 
     const handleMovieAdd = () =>{
-        console.log(name,genre,rating,release);
+        const movieData = {name,genre,rating,release_date:release}
+        fetch(allMovieUrl, {
+            method: 'POST',
+            headers: {
+                'Authorization': `JWT ${accessToken}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(movieData),
+        })
+            .then(res => {
+                if (res.status == 201) {
+                    Swal.fire("Successfully Created !", "", "success");
+                    return res.json();
+                } else {
+                    
+                    Swal.fire("An Error Occured !","","error");
+                }
+            })
+            .then(data => navigate(`/movie/${data.id}`))
     }
     return (
         <div className="bg-white bg-opacity-50 rounded-lg border py-8 w-2/3 mx-auto mt-10">
