6e43b703ff380e0109416c257cc5b5dd624e3130
Foysal Ahmed <mfoysal314@gmail.com>
2024-04-04 01:00:43 +0600

    movies are fetching from backend

--- src/Pages/Home/All Movies/AllMovie.jsx
+++ src/Pages/Home/All Movies/AllMovie.jsx
@@ -1,8 +1,24 @@
+import { allMovieUrl } from "@/Utilities/Url";
 import MovieCard from "@/components/MovieCard/MovieCard";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useEffect, useState } from "react";
 
 const AllMovie = () => {
+    const accessToken = localStorage.getItem('accessToken');
+    const [movies,setMovies] = useState([]);
+
+    useEffect(()=>{
+        fetch(allMovieUrl, {
+            method: 'GET',
+            headers: {
+                'Authorization': `JWT ${accessToken}`,
+                'Content-Type': 'application/json',
+            }
+        }).then(res => res.json())
+        .then(data => setMovies(data))
+    },[])
+
     return (
         <div>
             <div className="w-1/2 flex justify-center items-center gap-2 mx-auto mt-5">
@@ -10,32 +26,9 @@ const AllMovie = () => {
                 <Button className="bg-white text-black hover:text-white">Search</Button>
             </div>
             <div className="grid grid-cols-4 gap-3 mt-8">
-                <MovieCard/>
-                <MovieCard/>
-                <MovieCard/>
-                <MovieCard/>
-                <MovieCard/>
-                <MovieCard/>
-                <MovieCard/>
-                <MovieCard/>
-                <MovieCard/>
-                <MovieCard/>
-                <MovieCard/>
-                <MovieCard/>
-                <MovieCard/>
-                <MovieCard/>
-                <MovieCard/>
-                <MovieCard/>
-                <MovieCard/>
-                <MovieCard/>
-                <MovieCard/>
-                <MovieCard/>
-                <MovieCard/>
-                <MovieCard/>
-                <MovieCard/>
-                <MovieCard/>
-                <MovieCard/>
-                <MovieCard/>
+                {
+                    movies.map(movie => <MovieCard key={movie.id} movie={movie} />)
+                }
             </div>
         </div>
     );
--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -7,22 +7,22 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Link } from "react-router-dom";
 
-const MovieCard = () => {
+const MovieCard = ({movie}) => {
     return (
         <div className="bg-slate-300 rounded-lg border border-white bg-opacity-80 hover:bg-opacity-70 duration-300 py-3 px-4">
-            <h1 className="text-xl font-semibold capitalize">Avengers: Endgame</h1>
+            <h1 className="text-xl font-semibold capitalize">{movie.name}</h1>
             <div className="flex justify-between items-center my-3">
-                <h1 className="text-sm">Genre: Action</h1>
-                <h1 className="text-sm">Rating: 5.00 (10)</h1>
+                <h1 className="text-sm">Genre: {movie.genre}</h1>
+                <h1 className="text-sm">Rating: {movie.rating}</h1>
             </div>
-            <h1>Release Date: 10-05-2023</h1>
+            <h1>Release Date: {movie.release_date}</h1>
             <div className="mt-5 flex justify-between items-center">
                 <Popover>
                     <PopoverTrigger className="text-blue-800 text-sm hover:underline">
                         Provide Rating
                     </PopoverTrigger>
                     <PopoverContent>
-                        <h1 className="text-center font-medium">Provide Rating for <br /> Avengers Infinty War</h1>
+                        <h1 className="text-center font-medium">Provide Rating for <br /> {movie.name}</h1>
                         <div className="flex justify-center items-center gap-2 mt-5">
                             <Input type="number" max={5} min={1}></Input>
                             <Button>Submit</Button>
