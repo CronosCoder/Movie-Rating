c1ad30c11b5d9eb8a15cb6ee6c5c695675c3dff9
Foysal Ahmed <mfoysal314@gmail.com>
2024-04-04 02:05:31 +0600

    search section added

--- src/Pages/Home/All Movies/AllMovie.jsx
+++ src/Pages/Home/All Movies/AllMovie.jsx
@@ -7,6 +7,15 @@ import { useEffect, useState } from "react";
 const AllMovie = () => {
     const accessToken = localStorage.getItem('accessToken');
     const [movies,setMovies] = useState([]);
+    const [search,setSearch] = useState("");
+
+    const handleSearch = () =>{
+        if(search)
+        {
+            const searched = movies.filter(movie => movie.name.toLowerCase().includes(search.toLocaleLowerCase()));
+            setMovies(searched)
+        }
+    };
 
     useEffect(()=>{
         fetch(allMovieUrl, {
@@ -17,13 +26,13 @@ const AllMovie = () => {
             }
         }).then(res => res.json())
         .then(data => setMovies(data))
-    },[])
+    },[search])
 
     return (
         <div>
             <div className="w-1/2 flex justify-center items-center gap-2 mx-auto mt-5">
-                <Input placeholder="Search Movies by name" />
-                <Button className="bg-white text-black hover:text-white">Search</Button>
+                <Input onChange={(e)=>setSearch(e.target.value)} placeholder="Search Movies by name" />
+                <Button onClick={handleSearch} className="bg-white text-black hover:text-white">Search</Button>
             </div>
             <div className="grid grid-cols-4 gap-3 mt-8">
                 {
